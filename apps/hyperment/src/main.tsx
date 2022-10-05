18a7961ea4a96aa35802d293ef862e6e122f910f
thomson92 <t.kopaczyk@gmail.com>
2022-10-05 17:47:47 +0200

    init

--- apps/hyperment/src/main.tsx
+++ apps/hyperment/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import App from './app/app';
 
@@ -11,3 +12,8 @@ root.render(
     <App />
   </StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
